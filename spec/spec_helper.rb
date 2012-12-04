8977e56abeac508043ba893188fe0f62bd6c09fd
Ryo Nakamura <ryo-nakamura@cookpad.com>
2012-12-04 17:04:22 +0900

    Change rspec configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,5 +9,5 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_base_class_for_anonymous_controllers = false
-  config.order = "random"
+  config.render_views = true
 end
