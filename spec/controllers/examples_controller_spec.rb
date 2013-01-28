7508a8f2b41e1a736e7139373b90659d5d4abce0
Takayuki Kyowa <takayuki-kyowa@P285.local>
2013-01-28 23:57:24 +0900

    Skip path comment when format does not HTML

--- spec/controllers/examples_controller_spec.rb
+++ spec/controllers/examples_controller_spec.rb
@@ -2,39 +2,59 @@ require "spec_helper"
 
 describe ExamplesController do
   describe "#index" do
-    subject do
-      get :index
-    end
+    context "when render as HTML" do
+      subject do
+        get :index
+      end
 
-    context "when RenderWithPathComment is not attached" do
-      it "does not show partial view's relative path as HTML comment" do
-        should be_success
-        response.body.should_not include("<!-- BEGIN app/views/examples/_example.html.erb -->")
-        response.body.should_not include("<!-- END app/views/examples/_example.html.erb -->")
+      context "when RenderWithPathComment is not attached" do
+        it "does not show partial view's relative path as HTML comment" do
+          should be_success
+          response.body.should_not include("<!-- BEGIN app/views/examples/_example.html.erb -->")
+          response.body.should_not include("<!-- END app/views/examples/_example.html.erb -->")
+        end
+      end
+
+      context "when RenderWithPathComment is attached" do
+        before do
+          ViewSourceMap.attach
+        end
+
+        it "shows partial view's relative path as HTML comment" do
+          should be_success
+          response.body.should include("<!-- BEGIN app/views/examples/_example.html.erb -->")
+          response.body.should include("<!-- END app/views/examples/_example.html.erb -->")
+        end
+      end
+
+      context "when ViewSourceMap is detached" do
+        before do
+          ViewSourceMap.detach
+        end
+
+        it "does not show partial view's relative path as HTML comment" do
+          should be_success
+          response.body.should_not include("<!-- BEGIN app/views/examples/_example.html.erb -->")
+          response.body.should_not include("<!-- END app/views/examples/_example.html.erb -->")
+        end
       end
     end
 
-    context "when RenderWithPathComment is attached" do
-      before do
-        ViewSourceMap.attach
+    context "when render as TEXT" do
+      subject do
+        get "index", :format => "text"
       end
 
-      it "shows partial view's relative path as HTML comment" do
-        should be_success
-        response.body.should include("<!-- BEGIN app/views/examples/_example.html.erb -->")
-        response.body.should include("<!-- END app/views/examples/_example.html.erb -->")
-      end
-    end
+      context "when RenderWithPathComment is attached" do
+        before do
+          ViewSourceMap.attach
+        end
 
-    context "when ViewSourceMap is detached" do
-      before do
-        ViewSourceMap.detach
-      end
-
-      it "does not show partial view's relative path as HTML comment" do
-        should be_success
-        response.body.should_not include("<!-- BEGIN app/views/examples/_example.html.erb -->")
-        response.body.should_not include("<!-- END app/views/examples/_example.html.erb -->")
+        it "does not show partial view's relative path as HTML comment" do
+          should be_success
+          response.body.should_not include("<!-- BEGIN app/views/examples/_text.text.erb -->")
+          response.body.should_not include("<!-- END app/views/examples/_text.text.erb -->")
+        end
       end
     end
   end
