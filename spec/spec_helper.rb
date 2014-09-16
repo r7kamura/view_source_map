094d70ab13e119567149d2503666a1933020e1d5
Ryo Nakamura <r7kamura@gmail.com>
2014-09-16 10:38:08 +0900

    Change RSpec configuration to silent warning

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,5 +7,6 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
   config.infer_base_class_for_anonymous_controllers = false
+  config.infer_spec_type_from_file_location!
   config.render_views = true
 end
